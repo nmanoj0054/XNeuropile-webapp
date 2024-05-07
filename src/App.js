b66d46f54d9aa4f49b968b91a1005b95db3c3679
Suddhu-IITH2004 <suddhu040804@gmail.com>
2024-05-07 11:49:45 +0530

    Changed app size

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"XN-5 image.jpg"} className="App-logo" alt="logo" style={{borderRadius:"5rem"}}/>
         <p>
           {/* Edit <code>src/App.js</code> and save to reload.
            */}
