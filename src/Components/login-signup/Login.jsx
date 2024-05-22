fa80f71985589ed1e7fdf3691d4ac98c1bf49f28
nmanoj0054 <nayakmanoj4321@gmail.com>
2024-05-22 13:27:09 +0530

    chore: Add login and signup components with styles

---
+++ src/Components/login-signup/Login.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Login.css'
+
+import email_icon from '../Assets/email.png'
+import person_icon from '../Assets/person.png'
+import password_icon from '../Assets/password.png'
+
+const Login = () => {
+  return (
+    <div className='container'>
+      <div className='header'>
+        <div className='text'>Signup</div>
+        <div className='underline'></div>
+      </div>
+        <div className='inputs'>
+          <div className='input'>
+            <img src={person_icon} alt='name' className='icon'/>
+            <input type='text' placeholder='Username'/>
+          </div>
+          <div className='input'>
+            <img src={email_icon} alt='email' className='icon'/>
+            <input type='email' placeholder='Email'/>
+          </div>
+          <div className='input'>
+            <img src={password_icon} alt='password' className='icon'/>
+            <input type='password' placeholder='Password'/>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
