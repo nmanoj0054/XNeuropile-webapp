e61c0102315d79db126f59bea9f368ea6961f742
nmanoj0054 <nayakmanoj4321@gmail.com>
2024-05-22 14:07:57 +0530

    chore: Refactor login component and update app logo size

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from 'react';
+
 import './App.css';
+import Login from './Components/login-signup/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={"XN-5 image.jpg"} className="App-logo" alt="logo" style={{borderRadius:"30px",marginBottom:"50px"}}/>
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload.
-           */}
-           XNeuropile
-        </p>
-        <p>Happy Hacking!!!</p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='png'><img src={"XN-5 image.jpg"} className="App-logo" alt="logo" style={{borderRadius:"30px",marginBottom:"50px"}}/></div>
+      <div className='text'>XNeuropile</div>
+      <Login />
     </div>
   );
 }
--- src/Components/login-signup/Login.jsx
+++ src/Components/login-signup/Login.jsx
@@ -7,33 +7,32 @@ import password_icon from '../Assets/password.png'
 
 const Login = () => {
   return (
-    <div className='container'>
+    <div className='container' style={{ marginTop: "50px" }}>
       <div className='header'>
         <div className='text'>Signup</div>
         <div className='underline'></div>
       </div>
-        <div className='inputs'>
-          <div className='input'>
-            <img src={person_icon} alt='name' className='icon'/>
-            <input type='text' placeholder='Username'/>
-          </div>
-          <div className='input'>
-            <img src={email_icon} alt='email' className='icon'/>
-            <input type='email' placeholder='Email'/>
-          </div>
-          <div className='input'>
-            <img src={password_icon} alt='password' className='icon'/>
-            <input type='password' placeholder='Password'/>
-          </div>
+      <div className='inputs'>
+        <div className='input'>
+          <img src={person_icon} alt='name' className='icon'/>
+          <input type='text' placeholder='Username'/>
         </div>
-        <div className="forgot-password"> <span>Forgot Password?</span></div>
-        <div className="submit-container">
-          <div className="submit">Signup</div>
-          <div className="submit">Signup</div>
-
+        <div className='input'>
+          <img src={email_icon} alt='email' className='icon'/>
+          <input type='email' placeholder='Email'/>
         </div>
+        <div className='input'>
+          <img src={password_icon} alt='password' className='icon'/>
+          <input type='password' placeholder='Password'/>
+        </div>
+      </div>
+      <div className="forgot-password"> <span>Forgot Password?</span></div>
+      <div className="submit-container">
+        <div className="submit">Login</div>
+        <div className="submit">Signup</div>
+      </div>
     </div>
   )
 }
 
-export default Login
+export default Login;
