4ee68f89c1d56042b5918eaae9b2052c9fdcb9af
nmanoj0054 <nayakmanoj4321@gmail.com>
2024-05-22 14:17:57 +0530

    chore: Update app logo size and text style in App component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className='png'><img src={"XN-5 image.jpg"} className="App-logo" alt="logo" style={{borderRadius:"30px",marginBottom:"50px"}}/></div>
-      <div className='text'>XNeuropile</div>
+      <div className='text' style={{ marginTop: "20px" ,fontSize:"3em" }} >XNeuropile</div>
       <Login />
     </div>
   );
